1b93729a962a5774a00df6772cfa4e1437d73b1b
Gabrielpdsilva <gabriel.pimentel.sotc@gmail.com>
2020-10-06 13:58:53 -0300

    adding new line to matrix and new methods

--- src/controller/Deadlock.java
+++ src/controller/Deadlock.java
@@ -5,26 +5,30 @@ public class Deadlock {
 	boolean[] impasse = {false, false, false, false};
 	
 	static int[][] recursosAlocados = {
-										{3, 0, 1, 1},
-										{1, 1, 1, 0},
-										{1, 1, 0, 1},
-										{0, 0, 0, 0},
+										{0, 1, 0, 0},
+										{2, 0, 1, 1},
+										{0, 1, 0, 2},
+										{2, 0, 0, 0},
+										{0, 1, 0, 2},
 													};
 	
 	static int[][] recursosNecessarios = {
-										{1, 1, 0, 0},
-										{0, 1, 1, 2},
-										{3, 1, 0, 0},
-										{2, 1, 1, 0},
+										{1, 1, 0, 2},
+										{0, 2, 0, 1},
+										{1, 0, 2, 0},
+										{0, 2, 0, 2},
+										{2, 0, 1, 0}
 													};
 	
 	static int[] recursosExistentes = {7, 4, 2, 6};
 	
 	static int[] somatoriaRecursosAlocados = {4, 3, 1, 5};
 	
+	static int[] recursosDisponiveis;
+	
 	public static int[] pegaQtdRecursosEmUso(int[][] recursosAlocados){
 		
-		for(int linha = 0; linha < 4; linha++){
+		for(int linha = 0; linha < 5; linha++){
 			for(int coluna = 0; coluna < 4; coluna++){
 				somatoriaRecursosAlocados[coluna] += recursosAlocados[linha][coluna];
 			}
@@ -33,7 +37,7 @@ public class Deadlock {
 		return somatoriaRecursosAlocados;
 	}
 	
-	public boolean deadlock(boolean[] impasse){
+	public boolean deadlock(){
 		
 		if(impasse[0] && impasse[1] && impasse[2] && impasse[3])
 			return true;
@@ -51,17 +55,50 @@ public class Deadlock {
 		return recursosDisponiveis;
 	}
 	
-	public void usarRecurso(){
-		//TODO
+	public void usarRecurso(int linha){
+		for(int coluna = 0; coluna < 4; coluna++){
+
+			//recursos disponiveis estao sendo emprestados aos processos
+			recursosAlocados[linha][coluna] += recursosDisponiveis[coluna];
+			
+			//recursos disponiveis diminuiram
+			recursosDisponiveis[coluna] -= recursosAlocados[linha][coluna];
+		}
 	}
 	
-	public void devolverRecurso(){
-		//TODO
+	public void devolverRecurso(int linha){
+		for(int coluna = 0; coluna < 4; coluna++){
+			
+			//recursos antes usados agora serao devolvidos para os recursos disponiveis
+			recursosDisponiveis[coluna] += recursosAlocados[linha][coluna];
+			
+			//zerando a quantidade de recursos que aquele processo precisa
+			recursosAlocados[linha][coluna] = 0;
+		}
+	}
+	
+	public void compararRecursos(int[] recursosNecessarios, int[] recursosDisponiveis){
 		
+		if(deadlock())
+			return;
+		
+		for(int linha = 0; linha < 5; linha++){
+			for(int coluna = 0; coluna < 4; coluna++){
+						
+				if(recursosDisponiveis[coluna] < recursosNecessarios[coluna]){
+					impasse[linha] = true;
+				} else {
+					usarRecurso(linha);
+					devolverRecurso(linha);
+				}
+				
+			}	
+		}
 	}
 	
 	public static void main(String[] args) {
-		int[] recursosDisponiveis = calcularRecursosDisponiveis(recursosExistentes, somatoriaRecursosAlocados);
+		
+		recursosDisponiveis = calcularRecursosDisponiveis(recursosExistentes, somatoriaRecursosAlocados);
 		
 		for(int i = 0; i < 4; i++)
 			System.out.print(recursosDisponiveis[i] + " ");
